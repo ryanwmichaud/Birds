3cdc7c4fd598baf9b4fadfe426c064350c48c4d8
Ryan Michaud <ryanwmichaud@Ryans-MacBook-Air.local>
2025-02-05 23:06:44 -0600

    using state render instead of dom for answer classes. move sound click function to app to reduce prop drilling

--- frontend/src/components/Sound.tsx
+++ frontend/src/components/Sound.tsx
@@ -2,39 +2,24 @@ import { useEffect } from "react"
 
 export interface SoundProps {
     answer: string
-    isCorrect: boolean
-    setIsCorrect: React.Dispatch<React.SetStateAction<boolean>>
-    setAnswer: React.Dispatch<React.SetStateAction<string>>
-    setFileNum: React.Dispatch<React.SetStateAction<number>>
-    birds: string[]
     fileNum: number
+    isCorrect: boolean
+    handleSoundClick: ()=> void
 }
 
-export const Sound = ({answer, setAnswer, isCorrect, setIsCorrect, birds, fileNum, setFileNum}: SoundProps)=>{
+export const Sound = ({answer, fileNum, isCorrect, handleSoundClick}: SoundProps)=>{
 
     useEffect(()=>{
 
-    }, [answer])
-    const handleClick = ()=>{
-        if(isCorrect){
-            setIsCorrect(false)
-            setAnswer(birds[Math.floor(Math.random()*birds.length)])
-            setFileNum( Math.floor(Math.random() * 6))
-            const buttons = document.querySelectorAll('.choice')
-            buttons.forEach((button)=>{
-              button.classList.remove('correct', 'incorrect')
-            })
-        }else{
-            
-        }
-    }
+    }, [answer, fileNum])
+
 
     const path = `http://localhost:8000/audio/${encodeURIComponent(answer)}/${fileNum}.mp3`
 
     
 
     return(
-        <button className={`sound ${isCorrect ? 'correct' : ''}`} onClick={handleClick} >
+        <button className={`sound ${isCorrect ? 'correct' : ''}`} onClick={handleSoundClick} >
                 {isCorrect ? 
                 <>
                     <p>{`Correct - ${answer}`}</p>
